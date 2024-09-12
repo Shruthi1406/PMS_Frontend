3b92a06b769987fa0c972a84b7b3db5534189af6
ShruthiBurka <ShruthiBurka@DESKTOP-SIUQACR>
2024-09-12 16:03:03 +0530

    CoverPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import RegisterPatient from './components/RegisterPatient';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './App.css';
+//import RegisterPatient from './components/RegisterPatient';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import {createBrowserRouter,RouerProvider} from 'react-router-dom';
+//import Navbar from './components/Assests/Navbar';
+import Coverpage from './components/CoverPage/Coverpage';
+
+// const router=createBrowserRouter([
+//   {path:'/',element:<Navbar/>,
+// children:[
+//   {path:'/', element:<Navbar/>}
+// ]}
+// ])
 function App() {
   return (
     <div>
-      <RegisterPatient />
+      {/* <RegisterPatient />
+      <Navbar/> */}
+      <Coverpage/>
     </div>
   );
 }
