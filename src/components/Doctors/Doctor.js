329895fc6fd931fd07e7cc6ce6d25f0f8ffe7902
ShruthiBurka <ShruthiBurka@DESKTOP-SIUQACR>
2024-09-17 12:16:27 +0530

    Doctor

--- src/components/Doctors/Doctor.js
+++ src/components/Doctors/Doctor.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Doctor() {
   return (
-    <div>Doctor</div>
+    <div>
+        <div className="Doctors d-flex justify-content-center">
+            <div className="Dotor-image"><img src="..." class="img-fluid" alt="..."/></div>
+            <div className="Doctor-Details"></div>
+        </div>
+    </div>
   )
 }
 
