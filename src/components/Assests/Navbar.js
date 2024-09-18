13b2b5a9bcb87ac13d3e2e941080712044f826a9
SanthirajuMerla <SanthirajuMerla@DESKTOP-SE2TGKQ>
2024-09-18 12:30:12 +0530

    changes

--- src/components/Assests/Navbar.js
+++ src/components/Assests/Navbar.js
@@ -39,10 +39,6 @@ function Navbar() {
    for (i = 0; i < name.length; i += 1) {
      hash = name.charCodeAt(i) + ((hash << 5) - hash);
    } 
-  // name.charCodeAt() return an int between 0 and 65535
-  // left shift (<<)  operator moves to left by number of specified 
-  // bites after <<. The whole for loop will create a color hash 
-  // based on username length
    let color = '#';
  
    for (i = 0; i < 3; i += 1) {
