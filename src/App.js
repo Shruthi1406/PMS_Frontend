7bf5db6a498755c71c70627a4c991d2cc59cd900
PavaniMaddu <PavaniMaddu@DESKTOP-6TARQ1J>
2024-09-18 12:35:59 +0530

    merge4

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './components/Dashboard/Root';
 import Homepage from './components/Dashboard/Homepage';
 import Coverpage from './components/CoverPage/Coverpage';
-import Footer from './components/Footer';
 import Appointments from './components/appointment/Appointments'
 import PrivateRoute from './apiHandler/PrivateRoute';
 import MedicalHistoryForm from './components/Medicalhistory/Medicalhistory';
 import Hospital from './components/Hospitals/Hospital';
 import Receptionist from './components/receptionist/Receptionist'
 import Doctor from './components/Doctors/Doctor';
+
 const router = createBrowserRouter([ 
   {
     path:"/",
